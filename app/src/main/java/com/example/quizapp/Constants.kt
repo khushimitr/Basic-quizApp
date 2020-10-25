134ade9d51f2b7b033b2b2b943f30bfaf220e23d
khushimitr <khushi.mitr03@gmail.com>
2020-10-25 14:54:59 +0530

    First commit

---
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -0,0 +1,164 @@
+package com.example.quizapp
+
+object Constants {
+
+    const val USER_NAME:String = "username"
+    const val TOTAL_QUESTIONS : String = "total_questions"
+    const val CORRECT_ANSWERS : String = "correct_answers"
+
+
+    fun getQuestions(): ArrayList<Question> {
+        val questionList = ArrayList<Question>()
+
+        //1
+        val que1 =
+            Question(
+                1,
+                "What country does this flag belong to?",
+                R.drawable.ic_flag_of_argentina,
+                "Argentina",
+                "Australia",
+                "Armenia",
+                "Austria",
+                1
+            )
+        questionList.add(que1)
+
+        //2
+        val que2 =
+            Question(
+                1,
+                "What country does this flag belong to?",
+                R.drawable.ic_flag_of_australia,
+                "Armenia",
+                "Australia",
+                "United States of America",
+                "Fiji",
+                2
+            )
+        questionList.add(que2)
+
+
+        //3
+        val que3 =
+            Question(
+                1,
+                "What country does this flag belong to?",
+                R.drawable.ic_flag_of_brazil,
+                "Belarus",
+                "Belize",
+                "Brunei",
+                "Brazil",
+                4
+            )
+        questionList.add(que3)
+
+
+        //4
+        val que4 =
+            Question(
+                1,
+                "What country does this flag belong to?",
+                R.drawable.ic_flag_of_belgium,
+                "Bahamas",
+                "Belgium",
+                "Barbados",
+                "Belize",
+                2
+            )
+        questionList.add(que4)
+
+
+        //5
+        val que5 =
+            Question(
+                1,
+                "What country does this flag belong to?",
+                R.drawable.ic_flag_of_fiji,
+                "Gabon",
+                "France",
+                "Fiji",
+                "Finland",
+                3
+            )
+        questionList.add(que5)
+
+
+        //6
+        val que6 =
+            Question(
+                1,
+                "What country does this flag belong to?",
+                R.drawable.ic_flag_of_germany,
+                "Germany",
+                "Georgia",
+                "Greece",
+                "none of these",
+                1
+            )
+        questionList.add(que6)
+
+
+        //7
+        val que7 =
+            Question(
+                1,
+                "What country does this flag belong to?",
+                R.drawable.ic_flag_of_denmark,
+                "Dominica",
+                "Egypt",
+                "Denmark",
+                "Ethiopia",
+                3
+            )
+        questionList.add(que7)
+
+
+        //8
+        val que8 =
+            Question(
+                1,
+                "What country does this flag belong to?",
+                R.drawable.ic_flag_of_india,
+                "Ireland",
+                "Iran",
+                "Hungary",
+                "India",
+                4
+            )
+        questionList.add(que8)
+
+
+        //9
+        val que9 =
+            Question(
+                1,
+                "What country does this flag belong to?",
+                R.drawable.ic_flag_of_new_zealand,
+                "Australia",
+                "New Zealand",
+                "Tuvalu",
+                "United States of America",
+                2
+            )
+        questionList.add(que9)
+
+
+        //10
+        val que10 =
+            Question(
+                1,
+                "What country does this flag belong to?",
+                R.drawable.ic_flag_of_kuwait,
+                "Jordan",
+                "Sudan",
+                "Kuwait",
+                "Palestine",
+                3
+            )
+        questionList.add(que10)
+
+
+        return questionList
+    }
+}
